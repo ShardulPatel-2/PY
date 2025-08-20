238714ef9a01c679cc2fbbdb569001665a331be6
ShardulPatel-2 <spatel@dohacollege.com.qa>
2025-08-20 18:39:47 +0300

    Add files via upload

---
+++ polygon.py
@@ -0,0 +1,23 @@
+import turtle
+screen = turtle.Screen()
+
+
+
+
+board = turtle.Turtle()
+
+#Triangle
+board.forward(100)
+board.left(120)
+board.left(120)
+board.forward(100)
+
+#Square
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(100)
---
+++ turtle.py
@@ -0,0 +1,8 @@
+import turtle
+
+turtle.Screen().bgcolor("lightblue")
+
+sc = turtle.screen()
+sc.setup(400, 300)
+
+turtle .title("welcome to turtle window")
