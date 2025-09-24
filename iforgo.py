ac6679f5a7cf328c0903cbaac03a70ce17f37a8c
ShardulPatel-2 <spatel@dohacollege.com.qa>
2025-09-24 18:35:50 +0300

    Add files via upload

---
+++ iforgo.py
@@ -0,0 +1,11 @@
+file = open ('goon.txt' )
+Counter = 0
+Content = file.read()
+CoList = Content.split("\n")
+
+for i in list:
+    if i:
+        counter += 1
+
+print("Number of lines in the file is: ", counter)
+print("Counter")
